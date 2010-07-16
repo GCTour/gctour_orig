3c12eb0efc48381ec3a34a5b93b6315fbb9a5de1
Martin Georgi <maddin@madd.in>
2010-07-16 15:47:30 +0200

    Remove builds from repository and fixed some code styling in update.js

--- src/23-update.js
+++ src/23-update.js
@@ -1,45 +1,45 @@
-function update(){
+function update() {
 	var updateDate = eval(GM_getValue('updateDate'));
-	if(!updateDate){
+	if (!updateDate) {
 		updateDate = new Date();
-		GM_setValue('updateDate',uneval(updateDate));
+		GM_setValue('updateDate', uneval(updateDate));
 	}
 	var currentDate = new Date();
 
 	// if the last updateDate is more than 86 400 000 msec (1 day) ago - check for updates
-	if(currentDate.getTime() - updateDate.getTime() > 86400000){
+	if (currentDate.getTime() - updateDate.getTime() > 86400000) {
 		// set the new updateDate
-		GM_setValue('updateDate',uneval(currentDate));
+		GM_setValue('updateDate', uneval(currentDate));
 		// make the version request
-		var details = new Object();
+		var details = {};
 		details.method = 'GET';
 		details.url = 'http://gc.madd.in/gm/updates.xml';
-		details.onload = function(response) {parseUpdateXMLResponse(response.responseText)};
-		details.onerror = function(response) { alert('An error occour - please send an EMail to geocaching@madd.in!');};
+		details.onload = function (response) { parseUpdateXMLResponse(response.responseText); };
+		details.onerror = function (response) { alert('An update error occour - please send an EMail to geocaching@madd.in!');};
 		GM_xmlhttpRequest(details);
 	}
 }
 
-function parseUpdateXMLResponse(xmlString){
+function parseUpdateXMLResponse(xmlString) {
 	var updateNode;
 	var xmlDoc = (new DOMParser()).parseFromString(xmlString, "application/xml");
 	var string = '';
 
 	var scriptElements = xmlDoc.getElementsByTagName('script');
 
-	for(var i = 0;i< scriptElements.length;i++){
-		if ( scriptElements[i].getAttribute ('id') == scriptId){
+	for(var i = 0;i< scriptElements.length;i++) {
+		if (scriptElements[i].getAttribute('id') == scriptId) {
 			var versions = scriptElements[i].getElementsByTagName('version');
 			var currentVersion = 0; 
 			var currentVersionIndex; 
-			for(var j = 0;j< versions.length;j++){
-				if(versions[j].getAttribute('number') > currentVersion){
+			for(var j = 0;j< versions.length;j++) {
+				if (versions[j].getAttribute('number') > currentVersion) {
 					currentVersion = versions[j].getAttribute('number');
 					currentVersionIndex = j;
 				}
 			}
 
-			if (currentVersion > version){
+			if (currentVersion > version) {
 				updateNode = versions[currentVersionIndex];
 			}			
 		}		
@@ -48,12 +48,12 @@ function parseUpdateXMLResponse(xmlString){
 
 
 
-	if(updateNode){
-		var confirmString = 'There is a new version of GcTour.\n\t'+version+' -> '+updateNode.getAttribute('number')+'\nChanges:\n';
+	if (updateNode) {
+		var confirmString = 'There is a new version of GcTour.\n\t'+ version +' -> '+ updateNode.getAttribute('number') +'\nChanges:\n';
 
 		var changes = updateNode.getElementsByTagName('change');
-		for(var j = 0;j< changes.length;j++){
-			confirmString += '\t+ '+changes[j].textContent+'\n';
+		for(var j = 0;j< changes.length;j++) {
+			confirmString += '\t+ '+ changes[j].textContent +'\n';
 		}
 		confirmString += '\nDo you want to update?';
 		if (confirm(confirmString)) {

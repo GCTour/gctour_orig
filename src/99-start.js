17387a5c415b6239fdd5bb285357b76868cb3157
Martin Georgi <maddin@madd.in>
2010-07-21 19:24:20 +0200

    fixed version

--- src/99-start.js
+++ src/99-start.js
@@ -12,7 +12,7 @@ dumpProps(unsafeWindow.navigator);
 // test for firefox >= 3.5
 if (/Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)){ //test for Firefox/x.x or Firefox x.x (ignoring remaining digits);
  var ffversion=new Number(RegExp.$1) // capture x.x portion and store as a number
- if (ffversion < 5.5){
+ if (ffversion < 3.5){
     alert("Sorry, but you are running 'Firefox "+ffversion+"' which is not supported anymore.\nPlease update to 'Firefox 3.5' or above to use GCTour!");
  }
 }

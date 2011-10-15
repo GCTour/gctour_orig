624419af268a902f6866159a9baae52d238db491
Jens <gctour.dev.j@googlemail.com>
2011-10-16 00:11:02 +0200

    simple Debug Meldung ?ber den Status von jQuery und jQuery-UI

--- src/040-jquery.js
+++ src/040-jquery.js
@@ -1,3 +1,23 @@
 /*
 * set jquery and ui
 */
+
+(function () {
+
+  var str = "";
+  str += "jQuery und UI geladen = " + isjQuery;
+	if (isjQuery) {
+    str += "\n\tjQuery Version    = " + $.fn.jquery;
+    str += "\n\tjQueryUI Version  = " + $.ui.version;
+	}
+
+//  str += "\n\tisunsafeWindow.jQuery = " + isjQueryWindow;
+//  str += "\n\tunsafeWindow.jQuery Version = " + ((isjQueryWindow) ? unsafeWindow.jQuery.fn.jquery : "");
+
+  str += "\n\tBrowser Opera ?   = " + isOpera;
+//  str += "\n\tBrowser Mozilla ? = " + $.browser.mozilla;
+
+  debug(str);
+//  alert(str);
+
+})();

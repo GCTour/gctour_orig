1758ac362d3a89ce64bf71b35cb9693a6a7ccfdb
Jens <gctour.dev.j@googlemail.com>
2011-10-15 23:16:06 +0200

    DEBUG_MODE => GM_log Funktionen umgelagert und smaller Code

--- src/030-greasemonkey.js
+++ src/030-greasemonkey.js
@@ -1,3 +1,24 @@
 /*
 * greasemonkey settings and function
 */
+/* ----- DEBUG OUTPUT FUNCTIONS ------*/
+function toLog(typ,msg) {
+  if (DEBUG_MODE) {
+  	GM_log(typ + ": " + msg.toString());
+  }
+}
+
+function log(msg) {
+  toLog("Log",msg);
+}
+function debug(msg) {
+  toLog("DEBUG",msg);
+}
+
+function warn(msg) {
+  toLog("Warning",msg);
+}
+
+function error(msg) {
+  toLog("Error",msg);
+}

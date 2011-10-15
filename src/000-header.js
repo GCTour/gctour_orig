ee9aaf993f7fb72990f6084005695709a9333cd4
Jens <gctour.dev.j@googlemail.com>
2011-10-15 21:12:54 +0200

    add jQuery and jQuery-ui

--- src/000-header.js
+++ src/000-header.js
@@ -3,7 +3,8 @@
 // @namespace      madd.in
 // @version        @version@
 // @build          @build@
-// @description    Cachetour planing made easy. Pick some Caches, sort the list and print it out. Free for all users of geocaching.com! 
+// @description    Cachetour planing made easy. Pick some Caches, sort the list and print it out. Free for all users of geocaching.com!
+// @run-at         document-end
 // @include        http://www.geocaching.com/*
 // @include        https://www.geocaching.com/*
 // @include        http://gctour-spot.appspot.com*#gui
@@ -12,6 +13,8 @@
 // @copyright      2008, 2009, 2010, 2011 Martin Georgi
 // @author         madd.in
 // @icon           http://www.madd.in/geocaching/gm/gctourextension/icon.png
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// @require        https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 

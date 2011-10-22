c7fa38c8c641457f349b3f676dbda4a70d577ab1
Jens <gctour.dev.j@googlemail.com>
2011-10-22 16:32:13 +0200

    roll back: 500-cache-scraper.js not Revision in 163

--- src/500-cache-scraper.js
+++ src/500-cache-scraper.js
@@ -51,10 +51,9 @@ function getGeocacheFromElement(element){
 
 
 	// first check if really logged in 
-	    var fehler = test[229];
+	
 	//~ var logIOLink = dojo.query('a[id="ctl00_hlSignOut"]',element)[0].previousSibling.previousSibling;
-	//var logIOLink = dojo.query('a[id="ctl00_hlSignOutTTEST"]',element)[0].previousSibling.previousSibling;
-	var logIOLink = false;
+	var logIOLink = dojo.query('a[id="ctl00_hlSignOut"]',element)[0].previousSibling.previousSibling;
 	if(!logIOLink){
 		throw "No longer logged in.";
 	}

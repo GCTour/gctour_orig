d6953905feb4aa1ad1059e209339c22ce5ec5c62
gctour dev j <gctour.dev.j@gmail.com>
2011-11-24 16:56:06 +0000

    jslint test

--- src/150-own-marker.js
+++ src/150-own-marker.js
@@ -1,9 +1,35 @@
+function changeType(value,table,typeArray,staticMap){
+	return function(){
+		var trElement, i, tdElement;
+
+		document.getElementById('typeInput').value = value[0];
+		document.getElementById('typeInputSym').value = value[1];
+
+		staticMap.setIcon(value[0]);
+
+		table.innerHTML = "";
+
+		trElement = createElement('tr',{style:"height:27px"});	table.appendChild(trElement);
+		for( i = 0; i< 	typeArray.length ; i++ ){
+			tdElement = createElement('td',{style:"width:25px;"});
+			tdElement.style.cursor = 'pointer';
+			tdElement.style.padding = '0px';
+			tdElement.style.border = '1px solid silver';
+			tdElement.style.background ="url("+typeArray[i][0]+") center center no-repeat";
+			if (typeArray[i][0] == value[0]){tdElement.style.backgroundColor = '#B2D4F3';}
+			tdElement.addEventListener('click', changeType(typeArray[i],table,typeArray,staticMap), false);
+
+			trElement.appendChild(tdElement);
+		}
+	};
+}
+
 function showNewMarkerDialog(marker){
-    var overlayMarker, dangerDanger, anTable, tr, td, nameInput, cordsInputLat, cordsInputLon, cordsInput,
-        exampleCoords, staticGMap, staticGMapControl, zoomPlusButton, zoomMinusButton, contentTextarea,
-        markerTypeTable, typeInput, trElement, i , tdElement, cancel, submit, errors, makerName, markerContent,
-        markerType, markerTypeSym, latitude, longitude,markerPosition, markerPositionDelta, entry, latArray,
-        lonArray, latOrigin, lonOrigin;
+	var overlayMarker, dangerDanger, anTable, tr, td, nameInput, cordsInputLat, cordsInputLon, cordsInput,
+			exampleCoords, staticGMap, staticGMapControl, zoomPlusButton, zoomMinusButton, contentTextarea,
+			markerTypeTable, typeInput, trElement, i , tdElement, cancel, submit, errors, makerName, markerContent,
+			markerType, markerTypeSym, latitude, longitude,markerPosition, markerPositionDelta, entry, latArray,
+			lonArray, latOrigin, lonOrigin;
 
 	overlayMarker = getOverlay({caption:$.gctour.lang('printviewMarker'),minimized:true});
 
@@ -48,53 +74,39 @@ function showNewMarkerDialog(marker){
 	cordsInput.style.width = '350px';
 	cordsInput.style.marginRight = '5px';
 
-
 	var wptcodeInput = document.createElement('input');td.appendChild(wptcodeInput);
 	wptcodeInput.type = "hidden";
 	wptcodeInput.id = 'wptcodeInput';
 
-
-
-
-
 	exampleCoords = document.createElement('div');
-	exampleCoords.innerHTML = 	$.gctour.lang('example') + ' <i>N51° 12.123 E010° 23.123</i> or <i>51.123 10.123</i>'
+	exampleCoords.innerHTML = $.gctour.lang('example') + ' <i>N51° 12.123 E010° 23.123</i> or <i>51.123 10.123</i>';
 
 	td.appendChild(exampleCoords);
-
-
 
 	tr = document.createElement('tr');anTable.appendChild(tr);
 	td = document.createElement('td');tr.appendChild(td);
 	td = document.createElement('td');tr.appendChild(td);
 	td.align = 'left';
 
-
-
 	staticGMap = document.createElement('div');
 
-
 	var staticMap = new StaticMap(staticGMap,{});
-
 
 	var checkMarkerCoord = function(input){
 		return function(){
 			var coords = parseCoordinates(input.value);
 
-			if(coords == false){
+			if(coords === false){
 				cordsInput.style.backgroundColor = "#FF8888";
 			} else {
 				cordsInput.style.backgroundColor = "#88DC3B";
 				cordsInputLat.value = coords._lat;
 				cordsInputLon.value = coords._lon;
-	
-				
-				staticMap.setCoordinates(coords._lat,coords._lon);
-				
-			}
-		}
-	};
 
+				staticMap.setCoordinates(coords._lat,coords._lon);
+			}
+		};
+	};
 
 	cordsInput.addEventListener('keyup',checkMarkerCoord(cordsInput),false);
 	cordsInput.addEventListener('paste',checkMarkerCoord(cordsInput),false);
@@ -111,8 +123,6 @@ function showNewMarkerDialog(marker){
 	contentTextarea.id = 'markerContent';
 	contentTextarea.rows = '5';
 
-
-
 	// type buttons
 
 	tr = document.createElement('tr');anTable.appendChild(tr);
@@ -124,17 +134,17 @@ function showNewMarkerDialog(marker){
 	markerTypeTable = createElement('table',{style:"width:auto;"});td.appendChild(markerTypeTable);
 	markerTypeTable.id = 'markerType';
 
-	typeArray = new Array(
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/RedFlag.png','Red Flag'),
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/BlueFlag.png','Blue Flag'),
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/GreenFlag.png','Green Flag'),
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/Geocache.png','Geocache'),
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/GeocacheFound.png','Geocache Found'),
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/Information.png','Information'),
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/Park.png','Park'),
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/ParkingArea.png','Parking'),
-			new Array('http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/SkullAndBones.png','Skull And Crossbones')
-			);
+	typeArray = [
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/RedFlag.png','Red Flag'],
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/BlueFlag.png','Blue Flag'],
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/GreenFlag.png','Green Flag'],
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/Geocache.png','Geocache'],
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/GeocacheFound.png','Geocache Found'],
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/Information.png','Information'],
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/Park.png','Park'],
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/ParkingArea.png','Parking'],
+			['http://www.madd.in/geocaching/gm/gctourextension/map/icons/neu/SkullAndBones.png','Skull And Crossbones']
+	];
 	// iff we are editing a marker - so please set the right type
 	typeInput = document.createElement('input');
 	typeInput.id = 'typeInput';
@@ -162,7 +172,7 @@ function showNewMarkerDialog(marker){
 
 		tdElement.style.background ="url("+typeArray[i][0]+") center center no-repeat";
 		if(!marker){
-			if (i == 0) tdElement.style.backgroundColor = '#B2D4F3';
+			if (i === 0) { tdElement.style.backgroundColor = '#B2D4F3'; }
 			staticMap.setIcon(typeArray[0][0]);
 		} else {
 			if(typeArray[i][0] == marker.image){
@@ -179,10 +189,7 @@ function showNewMarkerDialog(marker){
 		trElement.appendChild(tdElement);
 	}
 
-
 	staticMap.hide();
-
-
 
 	// in the end please add a save and cancel button
 	tr = document.createElement('tr');anTable.appendChild(tr);
@@ -190,64 +197,56 @@ function showNewMarkerDialog(marker){
 	td.colSpan = '2';
 	td.align = 'right';
 
-
 	var buttonsDiv = createElement('div');append(buttonsDiv,overlayMarker);
 	buttonsDiv.setAttribute('class','dialogFooter');
-
 
 	cancel = createElement('input',{type:"button",value:$.gctour.lang('cancel'),style:"background-image:url("+closebuttonImage+")"});append(cancel,buttonsDiv);
 	cancel.addEventListener('click', closeOverlay, false);
 
-
 	submit = createElement('input',{type:"button",value:$.gctour.lang('save'),style:"background-image:url("+saveImage+")"});append(submit,buttonsDiv);
 
-
 	submit.addEventListener('click', function(){
-			errors = 0;
-			markerName = document.getElementById('markerName');
-			if (markerName.value != "") {
-			markerName.style.backgroundColor = "#FFFFFF";
-			} else {
-			markerName.style.backgroundColor = "#FF8888";
-			errors++;
-			}
-			markerCoords = document.getElementById('markerCoords');
+		errors = 0;
+		markerName = document.getElementById('markerName');
+		if (markerName.value != "") {
+		markerName.style.backgroundColor = "#FFFFFF";
+		} else {
+		markerName.style.backgroundColor = "#FF8888";
+		errors++;
+		}
+		markerCoords = document.getElementById('markerCoords');
 
-			if(markerCoords.style.backgroundColor != "rgb(136, 220, 59)"){
-			markerCoords.style.backgroundColor = "#FF8888";
-			errors++;
-			}
-			markerContent = document.getElementById('markerContent');
+		if(markerCoords.style.backgroundColor != "rgb(136, 220, 59)"){
+		markerCoords.style.backgroundColor = "#FF8888";
+		errors++;
+		}
+		markerContent = document.getElementById('markerContent');
 
-			markerType = document.getElementById('typeInput');
-			markerTypeSym = document.getElementById('typeInputSym');
-			if(errors != 0){
-				document.getElementById('dangerdanger').style.visibility = "visible";
-				return;
-			}
+		markerType = document.getElementById('typeInput');
+		markerTypeSym = document.getElementById('typeInputSym');
+		if(errors !== 0){
+			document.getElementById('dangerdanger').style.visibility = "visible";
+			return;
+		}
 
-			latitude =  document.getElementById('cordsInputLat').value*1;
-			longitude =  document.getElementById('cordsInputLon').value*1;
-			if(marker){
-				markerPosition = getPositionsOfId(marker.id);
-				markerPositionDelta = markerPosition - currentTour.geocaches.length +1;
-				deleteElementFunction((marker.id)?marker.id:marker.wptcode)();
-			} else {
-				markerPositionDelta = 0;
-			}
+		latitude =  document.getElementById('cordsInputLat').value*1;
+		longitude =  document.getElementById('cordsInputLon').value*1;
+		if(marker){
+			markerPosition = getPositionsOfId(marker.id);
+			markerPositionDelta = markerPosition - currentTour.geocaches.length +1;
+			deleteElementFunction((marker.id)?marker.id:marker.wptcode)();
+		} else {
+			markerPositionDelta = 0;
+		}
 
-			var wptCode =  document.getElementById('wptcodeInput').value;
+		var wptCode =  document.getElementById('wptcodeInput').value;
 
-			entry = addCustomMarker(markerName.value, latitude, longitude, markerContent.value, markerType.value, markerTypeSym.value,wptCode);
-			move(entry.id, markerPositionDelta);
+		entry = addCustomMarker(markerName.value, latitude, longitude, markerContent.value, markerType.value, markerTypeSym.value,wptCode);
+		move(entry.id, markerPositionDelta);
 
-			closeOverlay()
+		closeOverlay();
 
-	}
-
-	, false);
-
-
+	}, false);
 
 	// now set all previous values IFF a marker is given
 
@@ -268,69 +267,42 @@ function showNewMarkerDialog(marker){
 	nameInput.focus();
 }
 
+function updateMarkerOverviewMap(lat,lon,zoom){
+	var minZoom = 0,
+			maxZoom = 19,
+			apiKey = "ABQIAAAAKUykc2Tomn0DYkEZVrVaaRSNBTQkd3ybMgPO53QyT8hP9fzjBxSrEmDQGeGO-AZdQ4ogAvc8mRcV-g",
+			staticGMap;
+	// zoom out of range? please stop doing it ;-)
+	if(zoom < minZoom || zoom > maxZoom)  { return; }
+
+	debug("Updating map in marker window: " +lat + " " + lon + " Zoom:"+zoom);
+
+	staticGMap = document.getElementById('staticGMap');
+	staticGMap.style.display = 'block';
+	staticGMap.style.backgroundImage = 'url(http://maps.google.com/staticmap?sensor=false&size=350x200&zoom='+zoom+'&markers='+lat+','+lon+',midred&key='+apiKey+')';
+}
+
 function zoomInMarkerOverviewMap(){
 	return function(){
-        var staticGMap, zoom, lat, lon;
+		var staticGMap, zoom, lat, lon;
 
 		staticGMap = document.getElementById('staticGMap');
 	    zoom = staticGMap.style.backgroundImage.split('&zoom=')[1].split('&')[0];
 		lat = staticGMap.style.backgroundImage.split('&markers=')[1].split(',')[0];
 		lon = staticGMap.style.backgroundImage.split('&markers=')[1].split(',')[1];
 		updateMarkerOverviewMap(lat,lon,zoom-(-1));
-	}
+	};
 }
 
 function zoomOutMarkerOverviewMap(){
 	return function(){
-	    var staticGMap, zoom, lat, lon;
+    var staticGMap, zoom, lat, lon;
 
 		staticGMap = document.getElementById('staticGMap');
 	    zoom = staticGMap.style.backgroundImage.split('&zoom=')[1].split('&')[0];
 		lat = staticGMap.style.backgroundImage.split('&markers=')[1].split(',')[0];
 		lon = staticGMap.style.backgroundImage.split('&markers=')[1].split(',')[1];
 		updateMarkerOverviewMap(lat,lon,zoom-1);
-	}
+	};
 }
 
-
-function updateMarkerOverviewMap(lat,lon,zoom){
-	var minZoom = 0,
-	    maxZoom = 19,
-        apiKey = "ABQIAAAAKUykc2Tomn0DYkEZVrVaaRSNBTQkd3ybMgPO53QyT8hP9fzjBxSrEmDQGeGO-AZdQ4ogAvc8mRcV-g",
-        staticGMap;
-	// zoom out of range? please stop doing it ;-)
-	if(zoom < minZoom || zoom > maxZoom)
-		return;
-
-	debug("Updating map in marker window: " +lat + " " + lon + " Zoom:"+zoom);
-
-    staticGMap = document.getElementById('staticGMap');
-    staticGMap.style.display = 'block'
-	staticGMap.style.backgroundImage = 'url(http://maps.google.com/staticmap?sensor=false&size=350x200&zoom='+zoom+'&markers='+lat+','+lon+',midred&key='+apiKey+')';
-}
-
-function changeType(value,table,typeArray,staticMap){
-	return function(){
-	    var trElement, i, tdElement;
-
-		document.getElementById('typeInput').value = value[0];
-		document.getElementById('typeInputSym').value = value[1];
-
-		staticMap.setIcon(value[0]);
-
-		table.innerHTML = "";
-
-		trElement = createElement('tr',{style:"height:27px"});	table.appendChild(trElement);
-		for( i = 0; i< 	typeArray.length ; i++ ){
-			tdElement = createElement('td',{style:"width:25px;"});
-			tdElement.style.cursor = 'pointer';
-			tdElement.style.padding = '0px';
-			tdElement.style.border = '1px solid silver';
-			tdElement.style.background ="url("+typeArray[i][0]+") center center no-repeat";
-			if (typeArray[i][0] == value[0]){tdElement.style.backgroundColor = '#B2D4F3';}
-			tdElement.addEventListener('click', changeType(typeArray[i],table,typeArray,staticMap), false);
-
-			trElement.appendChild(tdElement);
-		}
-	}
-}

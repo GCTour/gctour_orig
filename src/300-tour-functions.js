f13a017c54f4dd5c42469f6c07dbeccea1c1ddc3
Jens <gctour.dev.j@googlemail.com>
2011-11-24 23:41:42 +0100

    jslint test

--- src/300-tour-functions.js
+++ src/300-tour-functions.js
@@ -1,6 +1,6 @@
 function getTourById(id){
-	currentTourId = GM_getValue('currentTour',-1);
-	tours = loadValue('tours',new Array());
+	var currentTourId = GM_getValue('currentTour',-1);
+	var tours = loadValue('tours', []);
 	for (var i = 0; i<tours.length;i++){
 		if(tours[i].id == currentTourId){
 			return tours[i];
@@ -16,32 +16,95 @@ function getNewTourId(){
 			tourId = tours[i].id + 1;
 		}
 	}
-
 	return tourId;
 }
 
 function isIdInTable(gcId){
-
-	for (var i = 0; i < currentTour.geocaches.length; i++)
-	{
+	for (var i = 0; i < currentTour.geocaches.length; i++) {
 		if(currentTour.geocaches[i].id == gcId){
 			return true;
 		}
 	}
-
 	return false;
 }
 
+function getPositionsOfId(theId){
+	for (var i = 0; i < currentTour.geocaches.length; i++){
+		if(currentTour.geocaches[i].id == theId || currentTour.geocaches[i].wptcode == theId){
+			return i;
+		}
+	}
+	return -1;
+}
+
+function checkOnlineConsistent(t){
+	// Creates way to much traffic :(
+	// maybe in the next version
+
+	/*
+
+	if(t.webcode){
+		geocaches = new Array();
+		waypoints = new Array();
+		costumMarkers = new Array();
+
+		var list = {webcode:t.webcode,geocaches:[]};
+		for (cache_i = 0; cache_i < t.geocaches.length; ++cache_i){
+			list.geocaches.push((typeof(t.geocaches[cache_i].latitude) != "undefined")?t.geocaches[cache_i].wptcode:t.geocaches[cache_i].id );
+		}
+
+		var jsonTour = JSON.stringify(list);
+		post(API_HOST+'/tour/check', "tour="+jsonTour,
+			function(text){
+				log("checkOnlineConsistent:"+text)
+
+				if(text == "false"){
+					if (document.getElementById("inconsistentTour")){
+						document.getElementById("inconsistentTour").style.display="inline";
+					} else {
+						window.setTimeout(function(){document.getElementById("inconsistentTour").style.display="inline"},3000);
+					}
+				} else {
+
+					if (document.getElementById("inconsistentTour")){
+						document.getElementById("inconsistentTour").style.display="none";
+					} else {
+						window.setTimeout(function(){document.getElementById("inconsistentTour").style.display="none"},3000);
+					}
+				}
+			}
+		);
+	} */
+}
+
+function saveTour(tour, notLoad){
+	var i;
+	for (i= 0; i < tours.length; ++i){
+		if(tours[i].id == tour.id){
+			tours[i] = tour;
+		}
+	}
+
+	GM_setValue('tours', JSON.stringify(tours));
+	if(notLoad === undefined){
+		GM_setValue('currentTour', tour.id);
+		log("updating "+tour.name);
+
+		checkOnlineConsistent(tour);
+	}
+}
+
+function saveCurrentTour(){
+	saveTour(currentTour);
+}
+
 function addNewTableCell(theEntry,effects){
-
 	var costumMarker = (typeof(theEntry.latitude) !== "undefined");
-
 
 	// if this is a cosutm marker user other id
 	var theId = (!costumMarker)?theEntry.id:theEntry.wptcode;
 
 	var entryLi = createElement('li', {id: theId, style: "position:relative;opacity:0;width:88%;list-style-image='url('"+theEntry.image+"');background-color:pink;"});
-	//~
 
 	// set the background image
 	//~ entryLi.style.background = "transparent url(http://stats.madd.in/counter/digit.php?digit="+(getPositionsOfId(theEntry.id || theEntry.wptcode)+1)+") fixed no-repeat bottom right";
@@ -52,12 +115,8 @@ function addNewTableCell(theEntry,effects){
 	//set the type
     entryLi.style.listStyleImage="url('"+theEntry.image+"')";
 
-
 	// make the gcid link
 	var nameCite = createElement('span',{style:"vertical-align:top"});
-
-
-
 
 	//~ var indexDiv = createElement('span',{style:"margin-right: 6px;"});
 		//~ indexDiv.innerHTML = "<b>"+(getPositionsOfId(theEntry.id || theEntry.wptcode)+1)+"</b>";
@@ -77,12 +136,10 @@ function addNewTableCell(theEntry,effects){
 		linkElement.textContent = theId;
 		nameCite.appendChild(linkElement);
 
-
 	} else {
 		nameCite.innerHTML += theEntry.name;
 		nameCite.style.textDecoration = "underline";
 	}
-
 
 	// the log/edit button and the delete button
 	var functionButtonsDiv = document.createElement('div');
@@ -104,7 +161,7 @@ function addNewTableCell(theEntry,effects){
 		editMarkerButton.title = $.gctour.lang('edit');
 		editMarkerButton.style.cursor = 'pointer';
 		editMarkerButton.src = editImageString;
-		editMarkerButton.addEventListener('click',  function(){showNewMarkerDialog(theEntry);}, false);
+		editMarkerButton.addEventListener('click', function(){showNewMarkerDialog(theEntry);}, false);
 		addOpacityEffects(editMarkerButton);
 		functionButtonsDiv.appendChild(editMarkerButton);
 	}
@@ -117,7 +174,6 @@ function addNewTableCell(theEntry,effects){
 	deleteImage.addEventListener('click', deleteElementFunction(theId), true);
 	addOpacityEffects(deleteImage);
 	functionButtonsDiv.appendChild(deleteImage);
-
 
 	// thanks to adam r
 	/* unneeded  since the list uses drag and drop
@@ -140,7 +196,6 @@ function addNewTableCell(theEntry,effects){
     upButton.src = upArrowImageString;
     upButton.addEventListener('click', moveUp(theId), true);
     addOpacityEffects(upButton);
-
 
 	var downButton = document.createElement('img');
     downButton.alt = "down";
@@ -167,10 +222,8 @@ function addNewTableCell(theEntry,effects){
 	upDownDiv.appendChild(bottomButton);
 	functionButtonsDiv.appendChild(upDownDiv);*/
 
-
 	entryLi.appendChild(functionButtonsDiv);
 	entryLi.appendChild(nameCite);
-
 
 	var nameDiv = document.createElement('div');
 	nameDiv.style.clear = 'left';
@@ -195,7 +248,7 @@ function addNewTableCell(theEntry,effects){
 	}
 
 	if(effects){
-		dojo.fadeIn({node: entryLi,duration: 1000}).play()
+		dojo.fadeIn({node: entryLi,duration: 1000}).play();
 	} else {
 		entryLi.style.opacity = "1";
 
@@ -203,27 +256,15 @@ function addNewTableCell(theEntry,effects){
 
 }
 
-
-function getPositionsOfId(theId){
-	for (var i = 0; i < currentTour.geocaches.length; i++){
-		if(currentTour.geocaches[i].id == theId || currentTour.geocaches[i].wptcode == theId){
-			return i;
-		}
-	}
-	return -1;
-}
-
-
 // function to move an cache to an given position in the list
 function move(id,positionDelta){
-
+  var i;
 
 	// locate the selected cache in the list
 	var position = getPositionsOfId(id);
 
-
 	// return if we are at the end or at top of the list!
-	if((position == 0  && positionDelta < 0) || (position == currentTour.geocaches.length-1 && positionDelta > 0) ){
+	if((position === 0  && positionDelta < 0) || (position == currentTour.geocaches.length-1 && positionDelta > 0) ){
 		return;
 	}
 
@@ -233,10 +274,10 @@ function move(id,positionDelta){
 	// remove it from the current geocaches
 	currentTour.geocaches.splice(position,1);
 
-	var tempCaches = new Array();
+	var tempCaches = [];
 
 	// first push all caches in front of the selected in the new array
-	for(var i = 0; i < position+positionDelta; i++){
+	for(i = 0; i < position+positionDelta; i++){
 		tempCaches.push(currentTour.geocaches[i]);
 	}
 
@@ -244,7 +285,7 @@ function move(id,positionDelta){
 	tempCaches.push(geoCache);
 
 	// and now the rest
-	for(var i = position+positionDelta; i < currentTour.geocaches.length; i++){
+	for(i = position+positionDelta; i < currentTour.geocaches.length; i++){
 		tempCaches.push(currentTour.geocaches[i]);
 	}
 
@@ -252,13 +293,12 @@ function move(id,positionDelta){
 	currentTour.geocaches = tempCaches;
 	saveCurrentTour();
 
-
 	// redraw the list:
 	var cacheList = document.getElementById('cacheList');
 
 	// just clear the list
-	while(cacheList.firstChild) cacheList.removeChild(cacheList.firstChild);
-	for (var i = 0; i < currentTour.geocaches.length; i++){
+	while(cacheList.firstChild) { cacheList.removeChild(cacheList.firstChild); }
+	for (i = 0; i < currentTour.geocaches.length; i++){
 		addNewTableCell(currentTour.geocaches[i],false);
 	}
 }
@@ -266,33 +306,64 @@ function move(id,positionDelta){
 function moveUp(id){
 	return function(){
 		move(id,-1);
-	}
+	};
 }
+
 function moveDown(id){
 	return function(){
 		move(id,1);
-	}
+	};
 }
 
 function moveTop(id){
 	return function(){
 		var position = getPositionsOfId(id);
 		move(id,-position);
-	}
+	};
 }
 
 function moveBottom(id){
 	return function(){
 		var position = getPositionsOfId(id);
 		move(id,currentTour.geocaches.length-position-1);
-	}
+	};
+}
+
+function saveNewCache(entry){
+	currentTour = getTourById(currentTourId);
+	currentTour.geocaches.push(entry);
+	saveCurrentTour();
+	log("saving "+ entry.id +" to "+currentTour.name);
+}
+
+function updateCacheCount(count){
+	dojo.query("span[id='cachecount']")[0].innerHTML = '('+count+')';
+
+	dojo.animateProperty(
+    {
+      node: "cachecount",duration: 1000,
+      properties: {
+        //~ color:         { start: "black", end: "white" },
+        backgroundColor:   { start: "#FFE000", end: "#FFF" }
+      }
+    }).play();
+
+    dojo.animateProperty(
+    {
+      node: "gctourButton",duration: 1000,
+      properties: {
+        //~ color:         { start: "black", end: "white" },
+        backgroundColor:   { start: "#FF0000", end: "#B2D4F3" }
+      }
+    }).play();
+
 }
 
 function addCustomMarker(name, lat, lon, content, typeImage, typeSymbol,wptcode){
 
-	if(currentTour.geocaches.length == 0){
+	if(!isNotEmptyList){
 		var table = document.getElementById('cacheList');
-		table.innerHTML ='';
+		table.innerHTML = '';
 	}
 
 	// customMarker:								e.g.:
@@ -303,11 +374,7 @@ function addCustomMarker(name, lat, lon, content, typeImage, typeSymbol,wptcode)
 	//		content	->	the content		"Test\nLINEBREAK"
 	//		symbol	->	GPX symbol name "Red Flag"
 
-
-
-
-	var entry = new Object();
-
+	var entry = {};
 
 	entry.wptcode = (wptcode)?wptcode:(new Date().getTime()-Math.round(lat+lon*1000)).toString(16);
 	entry.name = name;
@@ -322,12 +389,11 @@ function addCustomMarker(name, lat, lon, content, typeImage, typeSymbol,wptcode)
 	// add the newbie
 	addNewTableCell(entry,true);
 
-
 	// and make it persistence
 	saveNewCache(entry);
 
 	// update the cache count
-	updateCacheCount(currentTour.geocaches.length);;
+	updateCacheCount(currentTour.geocaches.length);
 
 	return entry;
 }
@@ -335,17 +401,18 @@ function addCustomMarker(name, lat, lon, content, typeImage, typeSymbol,wptcode)
 function addElementFunction(theId, theGuId, theName, theTypeImage){
 	return function () {
 
-		if(currentTour.geocaches.length == 0){
+		if(!isNotEmptyList){
 			var table = document.getElementById('cacheList');
 			table.innerHTML ='';
 		}
+
 		if(!isIdInTable(theId)){
 			// entry:								e.g.:
 			//		id		->	the gc.com id		GC00815
 			//		guid	->	the guid			6e974919-2b47-46e2-8661-3fc62a5a9650
 			//		name	->	the cachename		Echo the tomcat
 			//		image	->	the typeimage		http://www.geocaching.com/images/WptTypes/sm/2.gif
-			var entry = new Object();
+			var entry = {};
 			entry.id = theId;
 			//~ entry.name = theName.textContent;
 			entry.name = theName;
@@ -366,27 +433,82 @@ function addElementFunction(theId, theGuId, theName, theTypeImage){
 			updateCacheCount(currentTour.geocaches.length);
 
 		}
-	}
+	};
 }
 
-function saveNewCache(entry){
-	currentTour = getTourById(currentTourId);
-	currentTour.geocaches.push(entry);
-	saveCurrentTour();
-	log("saving "+ entry.id +" to "+currentTour.name);
+function deleteElementFunction(theId){
+  return function () {
+		var theElement = document.getElementById(theId);
+		//~ var trWithElement = theElement.parentNode.parentNode;
+
+		// cool sliding effect:
+		var elementRemoval = dojo.fadeOut({node: theElement,duration: 500});
+		dojo.connect(elementRemoval,"onEnd",function(){ theElement.parentNode.removeChild(theElement); });
+		elementRemoval.play();
+
+		// locate the element to delete
+		for (var i = 0; i < currentTour.geocaches.length; i++){
+			if(currentTour.geocaches[i].id == theId || currentTour.geocaches[i].wptcode == theId){
+				// array in js are dumb - where is removeAt ??
+				currentTour.geocaches.splice(i,1);
+				log("removing '"+theId +"' from '"+ currentTour.name+"'");
+				break;
+			}
+		}
+
+		saveCurrentTour();
+
+		// update the cache count
+		updateCacheCount(currentTour.geocaches.length);
+
+		if(!isNotEmptyList){
+			var table = document.getElementById('cacheList');
+			table.innerHTML = $.gctour.lang('emptyList');
+		}
+	};
+}
+
+function removeElementsFunction(descriptionElement, id, tagName){
+	return function () {
+		var elements = descriptionElement.getElementsByTagName(tagName);
+		for (var x = 0; x<elements.length; x++) {
+			if(elements[x].id == id){
+				elements[x].style.display = "none";
+			}
+		}
+	};
+}
+
+function updateTour(){
+	initCore();
+	updateGUI();
+}
+
+function loadTour(id){
+	return function(){
+		GM_setValue('currentTour',id);
+		if (document.getElementById("inconsistentTour")){
+			document.getElementById("inconsistentTour").style.display="none";
+		}
+
+		if(document.URL.search("webcode")>=0){
+			window.location = "http://www.geocaching.com";
+		} else {
+			updateTour();
+		}
+
+	};
 }
 
 function newTourFunction(preset){
 	return function(){
-		var newTour = new Object();
+		var newTour = {};
 		newTour.id = getNewTourId();
 
-		var tourName = (preset)?preset:"Tour "+newTour.id
+		var tourName = (preset) ? preset : "Tour " + newTour.id;
 		newTour.name = prompt($.gctour.lang('newTourDialog'), tourName);
-		newTour.geocaches = new Array();
-		if(!newTour.name) return false;
-
-
+		newTour.geocaches = [];
+		if(!newTour.name) { return false; }
 
 		tours.push(newTour);
 		log("Creating new tour: "+newTour.id +" ; "+ newTour.name);
@@ -397,57 +519,39 @@ function newTourFunction(preset){
 		updateTour();
 
 		return true;
-	}
+	};
 }
 
-function saveCurrentTour(){
-	saveTour(currentTour);
-}
+function deleteTourFunction(id, force){
+	return function(){
+		if (force || confirm($.gctour.lang('removeTourDialog'))) {
 
-function saveTour(tour, notLoad){
-	var i;
-	for (i= 0; i < tours.length; ++i){
-		if(tours[i].id == tour.id){
-			tours[i] = tour;
+			for (var i = 0; i < tours.length; i++){
+				if(tours[i].id == id){
+					log("removing '"+tours[i].name +"'");
+					// array in js are dumb - where is removeAt ??
+
+					var cachelist = document.getElementById('dialogDetails');
+
+					if(cachelist && cachelist.getAttribute("tourid") == tours[i].id){
+
+						showCacheList(currentTour)();
+						var loadButton = document.getElementById('loadButton');
+						loadButton.setAttribute("disabled","disabled");
+					}
+
+					dojo.destroy(dojo.byId("tour"+id));
+
+					tours.splice(i,1);
+					saveCurrentTour();
+
+					//updateTour();
+
+					break;
+				}
+			}
 		}
-	}
-
-
-
-	GM_setValue('tours', JSON.stringify(tours));
-	if(notLoad === undefined){
-		GM_setValue('currentTour', tour.id);
-		log("updating "+tour.name);
-
-		checkOnlineConsistent(tour);
-	}
-
-}
-
-function updateCacheCount(count){
-	dojo.query("span[id='cachecount']")[0].innerHTML = '('+count+')';
-
-	dojo.animateProperty(
-    {
-      node: "cachecount",duration: 1000,
-      properties: {
-        //~ color:         { start: "black", end: "white" },
-        backgroundColor:   { start: "#FFE000", end: "#FFF" }
-      }
-    }).play();
-
-
-    dojo.animateProperty(
-    {
-      node: "gctourButton",duration: 1000,
-      properties: {
-        //~ color:         { start: "black", end: "white" },
-        backgroundColor:   { start: "#FF0000", end: "#B2D4F3" }
-      }
-    }).play();
-
-
-
+	};
 }
 
 function deleteCurrentTour(){
@@ -464,157 +568,5 @@ function deleteCurrentTour(){
 
 		loadTour(nextTourId)();
 		deleteTourFunction(currentTourId, true)();
-	}
-}
-
-function deleteElementFunction(theId){
-   return function () {
-
-
-
-   				var theElement = document.getElementById(theId);
-				//~ var trWithElement = theElement.parentNode.parentNode;
-
-				// cool sliding effect:
-				var elementRemoval = dojo.fadeOut({node: theElement,duration: 500});
-				dojo.connect(elementRemoval,"onEnd",function(){ theElement.parentNode.removeChild(theElement); });
-				elementRemoval.play();
-
-				// locate the element to delete
-				for (var i = 0; i < currentTour.geocaches.length; i++){
-					if(currentTour.geocaches[i].id == theId || currentTour.geocaches[i].wptcode == theId){
-						// array in js are dumb - where is removeAt ??
-						currentTour.geocaches.splice(i,1);
-						log("removing '"+theId +"' from '"+ currentTour.name+"'");
-						break;
-					}
-				}
-
-				saveCurrentTour();
-
-				// update the cache count
-				updateCacheCount(currentTour.geocaches.length);
-
-
-				if(currentTour.geocaches.length == 0){
-					var table = document.getElementById('cacheList');
-					table.innerHTML = $.gctour.lang('emptyList');
-				}
-            }
-}
-
-
-
-function removeElementsFunction(descriptionElement, id, tagName){
-	return function () {
-		var elements = descriptionElement.getElementsByTagName(tagName);
-		for (var x = 0; x<elements.length; x++) {
-			if(elements[x].id == id){
-				elements[x].style.display = "none";
-			}
-		}
-	}
-}
-
-function loadTour(id){
-	return function(){
-		GM_setValue('currentTour',id);
-		if (document.getElementById("inconsistentTour")){
-			document.getElementById("inconsistentTour").style.display="none";
-		}
-
-		if(document.URL.search("webcode")>=0){
-			window.location = "http://www.geocaching.com";
-		} else {
-			updateTour();
-		}
-
-	}
-}
-
-function updateTour(){
-	initCore();
-	updateGUI();
-}
-
-
-
-function checkOnlineConsistent(t){
-	 // Creates way to much traffic :(
-	 // maybe in the next version
-
-	 /*
-
-	if(t.webcode){
-		geocaches = new Array();
-		waypoints = new Array();
-		costumMarkers = new Array();
-
-		var list = {webcode:t.webcode,geocaches:[]};
-		for (cache_i = 0; cache_i < t.geocaches.length; ++cache_i){
-			list.geocaches.push((typeof(t.geocaches[cache_i].latitude) != "undefined")?t.geocaches[cache_i].wptcode:t.geocaches[cache_i].id );
-		}
-
-
-
-		var jsonTour = JSON.stringify(list);
-		post(API_HOST+'/tour/check', "tour="+jsonTour,
-			function(text){
-				log("checkOnlineConsistent:"+text)
-
-				if(text == "false"){
-					if (document.getElementById("inconsistentTour")){
-						document.getElementById("inconsistentTour").style.display="inline";
-					} else {
-						window.setTimeout(function(){document.getElementById("inconsistentTour").style.display="inline"},3000);
-					}
-				} else {
-
-					if (document.getElementById("inconsistentTour")){
-						document.getElementById("inconsistentTour").style.display="none";
-					} else {
-						window.setTimeout(function(){document.getElementById("inconsistentTour").style.display="none"},3000);
-					}
-				}
-			}
-		);
-	} */
-}
-
-
-function deleteTourFunction(id, force){
-	return function(){
-		if (force || confirm($.gctour.lang('removeTourDialog'))) {
-
-			for (var i = 0; i < tours.length; i++){
-				if(tours[i].id == id){
-					log("removing '"+tours[i].name +"'");
-					// array in js are dumb - where is removeAt ??
-
-					var cachelist = document.getElementById('dialogDetails');
-
-
-
-					if(cachelist && cachelist.getAttribute("tourid") == tours[i].id){
-
-						showCacheList(currentTour)();
-						var loadButton = document.getElementById('loadButton');
-						loadButton.setAttribute("disabled","disabled");
-					}
-
-
-					dojo.destroy(dojo.byId("tour"+id));
-
-
-
-					tours.splice(i,1);
-					saveCurrentTour();
-
-					//updateTour();
-
-					break;
-				}
-			}
-		}
 	}
 }

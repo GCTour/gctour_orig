8c1b7dbcd7729b1b7ca40aeee01f7a473788bf97
gctour dev j <gctour.dev.j@gmail.com>
2011-11-25 12:58:53 +0000

    jslint test

--- src/600-setting-utilities.js
+++ src/600-setting-utilities.js
@@ -2,36 +2,36 @@ function setLanguage(l){
 	return function(){
 		GM_setValue('language',l);
 		window.location.reload();
-	}
+	};
 }
 
 function setGPXSchema(value){
 	return function(){
 		GM_setValue('gpxschema',value);
-	}
+	};
 }
 
 function toggleBoolValue(valueName, defaultValue){
 	return function(){
 		GM_setValue(valueName, !GM_getValue(valueName, defaultValue));
-	}
+	};
 }
 
 function  setPrintFontSize(fontSize){
 	return function(){
 		GM_setValue('printFontSize',fontSize);
-	}
+	};
 }
 
 function  setPrintMapType(mapType){
 	return function(){
 		GM_setValue('printOutlineMapType',mapType);
-	}
+	};
 }
 
 function  setPrintMapSize(mapSize){
 	return function(){
 		GM_setValue('defaultMapSize',mapSize);
-	}
+	};
 }
 

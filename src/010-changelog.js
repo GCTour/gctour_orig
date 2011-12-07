3956b7d4ee1ce530690c9ef8ceba20aecfce1895
Jens <gctour.dev.j@googlemail.com>
2011-12-07 01:04:01 +0100

    changelog

--- src/010-changelog.js
+++ src/010-changelog.js
@@ -2,6 +2,10 @@
  * Changelog:
  *
  * version @version@.@build@
+ *   - FIXED: exception SyntaxError JSON.parse unexpected character by download tour
+ *   - FIXED: exception TypeError progressBar is undefined
+ *
+ * version 2.1.11313
  *   - FIXED: GPX Download bug "...ctl00_hlSignOut... is undefined"
  *   - FIXED: Issue 18
  *   - FIXED: Update bug

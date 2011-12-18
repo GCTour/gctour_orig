119df818c8871274438bb3ad89ac23e024612a53
Martin Georgi <martin.georgi@web.de>
2011-12-18 14:10:50 +0100

    Es wird jetzt versucht auch das alte Appeningine Backend anzufragen, ob ein Webcode exisitiert. Ggf wird dann diese Tour zur?ck gegeben.

--- web/resources/tour_json.php
+++ web/resources/tour_json.php
@@ -25,12 +25,45 @@ class TourJSONResource extends Resource {
 		
 		$entitymanager = GeocacheManager::getInstance();
 		$tour = $entitymanager->fetchTour($this->webcode); 
-		$body = $tour->__toJSON();
-				
-		$response->code = Response::OK;
+		
+  		
+		
+		if(!isset($tour)){ // tour nicht in DB enthalten -> Frage bei appengine an.
+//		  $jsonurl = "http://192.168.178.10:8888/api/tour/".$this->webcode."/json";	
+		  $jsonurl = "http://gctour-spot.appspot.com/api/tour/".$this->webcode."/json";	  
+      $json = @file_get_contents($jsonurl,0,null,null);
+      if($json !== false){ // internal server error etc.
+        $json_output = json_decode($json);		
+        if(@$json_output->webcode === $this->webcode){ // tour not on server
+    		  $body = json_encode($json_output);
+    		} else { // weder in db von madd.in noch GAE eine tour zum Webcode gefunden!
+    		  $output = array();
+    		  $output['type'] = "error";
+    		  $output['message'] = "no tour";
+    		  $output['webcode'] = $this->webcode;
+    		  $body = json_encode((object)$output);
+    		}
+      } else { // damn appengine throws some error
+        $output = array();
+  		  $output['type'] = "error";
+  		  $output['message'] = "no tour";
+  		  $output['webcode'] = $this->webcode;
+  		  $body = json_encode((object)$output);
+      }
+  	} else {
+  	  $body = $tour->__toJSON();
+  	}
+  	
+  	$response->code = Response::OK;
 		$response->addHeader('Content-type', 'text/html');
 		$response->addHeader('charset', 'utf-8');
-		$response->body = $body;  
+		$response->body = $body;
+  	
+  		
+  /*
+		$body = $tour->__toJSON();
+				
+		  */
 	
 	   
 		

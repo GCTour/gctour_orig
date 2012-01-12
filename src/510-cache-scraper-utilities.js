6aa0f4197470c63c8d5825358f8b07e0b6c50c41
Jens <gctour.dev.j@googlemail.com>
2012-01-13 00:23:56 +0100

    510... Dojo to jQuery (two open)

--- src/510-cache-scraper-utilities.js
+++ src/510-cache-scraper-utilities.js
@@ -3,22 +3,23 @@ function getMinimalGeocacheDetails(detailsPage){
   /* gcid, cacheid, guid, typeimage, name */
 
   var geocache_details = {};
-
+  var $obj = {}; // temp jquery container
 
   /* GCCode Stand 07.10.2011
    * <span id="ctl00_ContentBody_CoordInfoLinkControl1_uxCoordInfoCode" class="CoordInfoCode">GC2HFRB</span> <--- holen über 'class'!
-   * Fallback#1: <input type="submit" name="ctl00$ContentBody$btnSendToPhone" value="Send to My Phone" onclick="s2phone(&#39;GC2HFRB&#39;);return false;" id="ctl00_ContentBody_btnSendToPhone" />
-   * Fallback#2: steht im HEAD: <meta property="og:url" content="http://coord.info/GC2HFRB" name="og:url">*/
-  var $arr =[
-    $('.CoordInfoCode').first(),
-    $('input#ctl00_ContentBody_btnSendToPhone').first(), // Fallback #1
-    $('meta[name="og:url"]').first()                     // Fallback #2
+   * Fallback#1: nur wenn eingeloggt: <input type="submit" name="ctl00$ContentBody$btnSendToPhone" value="Send to My Phone" onclick="s2phone(&#39;GC2HFRB&#39;);return false;" id="ctl00_ContentBody_btnSendToPhone" />
+   * Fallback#2: steht im HEAD: <meta property="og:url" content="http://coord.info/GC2HFRB" name="og:url">
+  */
+  $obj.gcc = [
+    $('.CoordInfoCode', detailsPage).first(),
+    $('input#ctl00_ContentBody_btnSendToPhone', detailsPage).first(), // Fallback #1
+    $('meta[name="og:url"]', detailsPage).first()                     // Fallback #2
   ];
 
   geocache_details.gccode =
-    ($arr[0].length && $.trim($arr[0].text())) ||
-    ($arr[1].length && $.trim($arr[1].attr('onclick').split("'")[1])) ||
-    ($arr[2].length && $.trim($arr[2].attr('content').split('/')[3])) ||
+    ($obj.gcc[0].length && $.trim($obj.gcc[0].text())) ||
+    ($obj.gcc[1].length && $.trim($obj.gcc[1].attr('onclick').split("'")[1])) ||
+    ($obj.gcc[2].length && $.trim($obj.gcc[2].attr('content').split('/')[3])) ||
     null;
 
   if (!geocache_details.gccode)  {
@@ -26,64 +27,90 @@ function getMinimalGeocacheDetails(detailsPage){
   } else {
     debug(
       "getMinimalGeocacheDetails - GCCode: " + geocache_details.gccode + "\n" +
-      "\t1: " + ( ($arr[0].length) ? $arr[0].text() : "null" ) + "\n" +
-      "\t2: " + ( ($arr[1].length) ? $arr[1].attr('onclick').split("'")[1] : "null" ) + "\n" +
-      "\t3: " + ( ($arr[2].length) ? $arr[2].attr('content').split('/')[3] : "null" )
+      "\t1: " + ( ($obj.gcc[0].length) ? $obj.gcc[0].text() : "null" ) + "\n" +
+      "\t2: " + ( ($obj.gcc[1].length) ? $obj.gcc[1].attr('onclick').split("'")[1] : "null" ) + "\n" +
+      "\t3: " + ( ($obj.gcc[2].length) ? $obj.gcc[2].attr('content').split('/')[3] : "null" )
     );
   }
 
 
-  /* suche die CacheId aus einer diesen Quellen:
+  /* CacheId aus einer diesen Quellen:
    *  HTML          REGEXP          Quelle
     ccid=1957539"      ccid=(\d+)        "View all Trackables" Link
-    "CacheID":1957539     \"CacheID\":(\d+)    teil der vorgeladenen Logs
-    w=1957539"        \Ww=(\d+)        "Watch Listing" link
+    "CacheID":1957539  \"CacheID\":(\d+) teil der vorgeladenen Logs
+    w=1957539"         \Ww=(\d+)         "Watch Listing" link
   */
   try{
     var cacheid_regex = /ccid=\d+|\"CacheID\":\d+|\Wws=\d+/;
     var cacheid_arr = cacheid_regex.exec(detailsPage.innerHTML);
     geocache_details.cacheid = cacheid_arr[0].split(/:|=/)[1];
-    debug("getMinimalGeocacheDetails - CacheID:"+geocache_details.cacheid);
+    debug("getMinimalGeocacheDetails - CacheID:" + geocache_details.cacheid);
   } catch(e){
-    throw "Error getting 'cacheid' from "+geocache_details.gccode;
+    throw "Error getting 'cacheid' from " + geocache_details.gccode;
   }
 
 
-  /* hole den Cachenamen:
+  /* Cachename:
    *  <span id="ctl00_ContentBody_CacheName">3, 2, 1 ... Lift-Off</span></h2>
-   *   <meta name="og:title" content="3, 2, 1 ... Lift-Off" property="og:title" />
-   */
-  try{
-    geocache_details.name = trim(dojo.query('span[id="ctl00_ContentBody_CacheName"]',detailsPage)[0].textContent);
-  } catch(e){
-    try{
-      geocache_details.name = dojo.query('meta[name="og:title"]',detailsPage)[0].getAttribute('content');
-    } catch(e){
-      throw "Error getting 'name' from "+geocache_details.gccode;
-    }
+   *  <meta name="og:title" content="3, 2, 1 ... Lift-Off" property="og:title" />
+  */
+  $obj.name = [
+    $('span#ctl00_ContentBody_CacheName', detailsPage).first(),
+    $('meta[name="og:title"]', detailsPage).first() // Fallback #1
+  ];
+
+  geocache_details.name =
+    ($obj.name[0].length && $.trim($obj.name[0].text())) ||
+    ($obj.name[1].length && $.trim($obj.name[1].attr('content'))) ||
+    null;
+
+  if (!geocache_details.name) {
+    throw "Error getting 'cacheName' from " + geocache_details.gccode;
+  } else {
+    debug(
+      "getMinimalGeocacheDetails - Name: " + geocache_details.name + "\n" +
+      "\t1: " + ( ($obj.name[0].length) ? $obj.name[0].text() : "null" ) + "\n" +
+      "\t2: " + ( ($obj.name[1].length) ? $obj.name[1].attr('content') : "null" )
+    );
   }
-  debug("getMinimalGeocacheDetails - Name:"+geocache_details.name);
 
 
   /* Hole guid
+   *  <form name="aspnetForm" method="post" action="cache_details.aspx?guid=712fed16-77ab-48f4-a269-18cc27bb2a14" onsubmit="javascript:return WebForm_OnSubmit();" id="aspnetForm">
    *  <a id="ctl00_ContentBody_lnkPrintFriendly5Logs" href="cdpf.aspx?guid=712fed16-77ab-48f4-a269-18cc27bb2a14&amp;lc=5" target="_blank">5 Logs</a>&nbsp;
+   *  <a id="ctl00_ContentBody_uxTravelBugList_uxTrackableItemsHistory" href="../track/search.aspx?wid=712fed16-77ab-48f4-a269-18cc27bb2a14">View past Trackables</a>
+   *  <a id="ctl00_ContentBody_uxLogbookLink" href="cache_logbook.aspx?guid=712fed16-77ab-48f4-a269-18cc27bb2a14">View Logbook</a>
    *  lat=51.167083; lng=10.533383; guid='712fed16-77ab-48f4-a269-18cc27bb2a14';
-   */
+  */
+  $obj.guid = [
+    $("form[name='aspnetForm']", detailsPage).first(),
+    $("a#ctl00_ContentBody_lnkPrintFriendly5Logs", detailsPage).first(),
+    $("a#ctl00_ContentBody_uxTravelBugList_uxTrackableItemsHistory", detailsPage).first(),
+    $("a#ctl00_ContentBody_uxLogbookLink", detailsPage).first()
+  ];
 
-   try{
-     geocache_details.guid = dojo.query("a[id='ctl00_ContentBody_lnkPrintFriendly5Logs']",detailsPage)[0].href.split("guid=")[1].split("&")[0];
-   } catch(e){
-    try{
-      var guid_regex = /guid='(.*)'/;
-      var guid_arr = guid_regex.exec(detailsPage.innerHTML);
-      geocache_details.guid = guid_arr[1];
-    } catch(e){
-      throw "Error getting 'guid' from "+geocache_details.gccode;
-    }
-   }
-   debug("getMinimalGeocacheDetails - Guid:"+geocache_details.guid);
+  geocache_details.guid =
+    ($obj.guid[0].length && $.trim($obj.guid[0].attr("action").split("guid=")[1].split("&")[0])) ||
+    ($obj.guid[1].length && $.trim($obj.guid[1].attr("href").split("guid=")[1].split("&")[0])) ||
+    ($obj.guid[2].length && $.trim($obj.guid[2].attr("href").split("wid=")[1].split("&")[0])) ||
+    ($obj.guid[3].length && $.trim($obj.guid[3].attr("href").split("guid=")[1].split("&")[0])) ||
+    null;
 
+  if (!geocache_details.guid) {
+    throw "Error getting 'guid' from " + geocache_details.gccode;
+  } else {
+    debug(
+      "getMinimalGeocacheDetails - Guid: " + geocache_details.guid + "\n" +
+      "\t1: " + ( ($obj.guid[0].length) ? $obj.guid[0].attr("action").split("guid=")[1].split("&")[0] : "null" ) + "\n" +
+      "\t2: " + ( ($obj.guid[1].length) ? $obj.guid[1].attr("href").split("guid=")[1].split("&")[0] : "null" ) + "\n" +
+      "\t3: " + ( ($obj.guid[2].length) ? $obj.guid[2].attr("href").split("wid=")[1].split("&")[0] : "null" ) + "\n" +
+      "\t4: " + ( ($obj.guid[3].length) ? $obj.guid[3].attr("href").split("guid=")[1].split("&")[0] : "null" )
+    );
+  }
 
+  /* Hole type
+   *
+  */
    try{
      geocache_details.type = dojo.query('meta[name="og:imageXX"]',detailsPage)[0].getAttribute("content").split("/")[6].split('.')[0]+".gif";
    } catch(e){
@@ -96,19 +123,42 @@ function getMinimalGeocacheDetails(detailsPage){
    debug("getMinimalGeocacheDetails - Type:"+geocache_details.type);
 
 
+  /* Todo vorhergehendes zu nachfolgendes ändern und debuggen
+
+  $obj.type = [
+    $('meta[name="og:imageXX"]', detailsPage).first(),
+    $('a[title="About Cache Types"] > img', detailsPage).first()
+  ];
+
+  geocache_details.type =
+    ($obj.type[0].length && $.trim($obj.type[0].attr("content").split("/")[6].split('.')[0] + ".gif"))||
+    ($obj.type[1].length && $.trim($obj.type[1].attr("src").split("/")[5])) ||
+    null;
+
+  if (!geocache_details.type) {
+    throw "Error getting 'type' from " + geocache_details.gccode;
+  } else {
+    debug(
+      "getMinimalGeocacheDetails - Type: " + geocache_details.type + "\n" +
+      "\t1: " + ( ($obj.type[0].length) ? $obj.type[0].attr("content").split("/")[6].split('.')[0] + ".gif" : "null" ) + "\n" +
+      "\t2: " + ( ($obj.type[1].length) ? $obj.type[1].attr("src").split("/")[5] : "null" )
+    );
+  }
+  */
+
   return geocache_details;
 
 }
 
 function getLogs(userToken, maxLogsCount){
-  maxLogsCount = maxLogsCount || 25; // optionaler Parameter default = 25 
+  maxLogsCount = maxLogsCount || 25; // optionaler Parameter default = 25
   var i = 1,
       numLogsPages = (maxLogsCount < 100) ? 25 : 100,
       logs = [],
       urlTemplate = 'http://www.geocaching.com/seek/geocache.logbook?tkn='+userToken+'&idx=#PAGE#&num=#NUM#&decrypt=false',
       url, n,
       log_obj = {},
-      req = new XMLHttpRequest(), 
+      req = new XMLHttpRequest(),
       booA, booB;
 
   do {
@@ -150,13 +200,13 @@ function getLogs(userToken, maxLogsCount){
     //~ GroupImageUrl  "/images/icons/reg_user.gif"
     //~ Images  []
     //~ debug(logs[0].UserName);
-  
+
     i++;
     booA = (i <= log_obj.pageInfo.totalPages); // gibt es noch eine Seite danach ?
     booB = (logs.length < maxLogsCount);       // maximale gewünschte Anzahl Logs noch nicht erreicht ?
-  
+
   } while (booA && booB); // nächster Request ?
-    
+
   // LogArray ggf. kürzen
   if (logs.length > maxLogsCount) {
       n = maxLogsCount - logs.length;

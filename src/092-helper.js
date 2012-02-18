f1096fa5a55e6e284649cfe4a6e1a8058f2c97a8
Martin Georgi <martin.georgi@web.de>
2012-02-18 10:10:36 +0100

    getMapCenterAndRadius geht jetzt wieder.

--- src/092-helper.js
+++ src/092-helper.js
@@ -4,25 +4,28 @@
 // googleMap center and radius: return object center and radius
 var getMapCenterAndRadius = function(){
 
-  var googleMap = unsafeWindow.map,
+  var googleMap = unsafeWindow.MapSettings.Map,
       ret = {},
       bounds;
 
   ret.center = "";
   ret.radius = "";
-
+  
+  
   if ( typeof(googleMap) !== "undefined" ) {
+    
     bounds = googleMap.getBounds();
     ret.center = googleMap.getCenter();
+    
     ret.radius = Math.floor(
       distanceBetween(
-        ret.center.lat(), ret.center.lng(),
-        bounds.getNorthEast().lat(),
-        bounds.getNorthEast().lng() - (bounds.getNorthEast().lng() - bounds.getSouthWest().lng()) / 2
+        ret.center.lat, ret.center.lng,
+        bounds.getNorthEast().lat,
+        bounds.getNorthEast().lng - (bounds.getNorthEast().lng - bounds.getSouthWest().lng) / 2
       )
     ) / 1000;
   }
-
+  
   return ret;
 };
 

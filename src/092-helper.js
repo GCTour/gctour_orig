6c957022d07e23bd20dc5edbce62d53b12e2a65b
Jens <gctour.dev.j@googlemail.com>
2012-05-11 22:34:13 +0200

    minimale code .replace()         str = str.replace(1);         str = str.replace(2);         =         str = str.replace(1).replace(2);

--- src/092-helper.js
+++ src/092-helper.js
@@ -33,8 +33,8 @@ var getMapCenterAndRadius = function(){
 // fn from js-Framework prototype v1.7
 var isJSON = function (str) {
   if (str.length === 0) { return false; }
-  str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@');
-  str = str.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
-  str = str.replace(/(?:^|:|,)(?:\s*\[)+/g, '');
+  str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@')
+           .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+           .replace(/(?:^|:|,)(?:\s*\[)+/g, '');
   return (/^[\],:{}\s]*$/).test(str);
 };

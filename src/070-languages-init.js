04bb415bd6aeec265d0352f5e54ece5b9d20a809
Jens <gctour.dev.j@googlemail.com>
2012-05-15 21:37:07 +0200

    redesign function $.gctour.lang -> new option languageString "dialog.settings" with dot notation

--- src/070-languages-init.js
+++ src/070-languages-init.js
@@ -14,26 +14,41 @@
     is language or str undefined = return ""
   */
   $.gctour.lang = function(str){
+    var i18n = $.gctour.i18n,
+        cur  = $.gctour.currentLang,
+        def  = $.gctour.defaultLang,
+        i, arr, lang, trans, cur_trans, def_trans;
 
-    var i18n  = $.gctour.i18n,
-        cur   = $.gctour.currentLang,
-        def   = $.gctour.defaultLang,
-        trans = (i18n[cur] && i18n[cur][str]) ||  // current
-                (i18n[def] && i18n[def][str]) ||  // default
+    // Übersetzungssuchstring ggf. splitten für Objekte
+    arr = str.split(".");
+
+    cur_trans = i18n[cur] || false;
+    def_trans = i18n[def] || false;
+
+    // Versuch die Übersetzung zu holen
+    // 'boolean' : false => wird richtig ausgegeben
+    for (i = 0; i < arr.length; i++) {
+      cur_trans = (cur_trans && (cur_trans[arr[i]] !== undefined)) ? cur_trans[arr[i]] : undefined;
+      def_trans = (def_trans && (def_trans[arr[i]] !== undefined)) ? def_trans[arr[i]] : undefined;
+    }
+
+    // Check ob Übersetzung erfolgreich geholt werden konnte
+    trans = (cur_trans !== undefined) ? cur_trans :
+              (def_trans !== undefined) ? def_trans :
                 ((DEBUG_MODE === true) ? "NO LANGUAGE" : "");
 
     // debug info current language
     if (!i18n[cur]) {
       debug("ERROR: language '" + cur + "' is undefined");
-    } else if (!i18n[cur][str]) {
+    } else if (cur_trans === undefined) {
       debug("ERROR: active language (" + cur + "), search '" + str + "' is undefined");
     }
 
     // debug info current language
     if (!i18n[def]) {
       debug("ERROR: language '" + def + "' is undefined");
-    } else if (!i18n[def][str]) {
-      debug("ERROR: default language (" + cur + "), search '" + str + "' is undefined");
+    } else if (def_trans === undefined) {
+      debug("ERROR: default language (" + def + "), search '" + str + "' is undefined");
     }
 
     return trans;

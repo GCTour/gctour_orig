51c0bf1c8ff157da0ca19b82034854f124a5d62c
Jens <gctour.dev.j@googlemail.com>
2012-05-18 22:32:23 +0200

    fn getMinimalGeocacheDetails => GCCode mit RegEx mit helper fn isGCCode und findGCCodeFromString

--- src/092-helper.js
+++ src/092-helper.js
@@ -10,13 +10,13 @@ var getMapCenterAndRadius = function(){
 
   ret.center = "";
   ret.radius = "";
-  
-  
+
+
   if ( typeof(googleMap) !== "undefined" ) {
-    
+
     bounds = googleMap.getBounds();
     ret.center = googleMap.getCenter();
-    
+
     ret.radius = Math.floor(
       distanceBetween(
         ret.center.lat, ret.center.lng,
@@ -25,7 +25,7 @@ var getMapCenterAndRadius = function(){
       )
     ) / 1000;
   }
-  
+
   return ret;
 };
 
@@ -37,4 +37,21 @@ var isJSON = function (str) {
            .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
            .replace(/(?:^|:|,)(?:\s*\[)+/g, '');
   return (/^[\],:{}\s]*$/).test(str);
+};
+
+// is String a GCCode ?
+// begin with 'GC' + 1 to 6 chars, current is 5 (05.2012)
+// return Boolean
+var isGCCode = function (gccode) {
+  return (/^\s*(GC[0-9A-Z]{1,6})\s*$/).test(gccode);
+};
+
+// find GCID (GCCode) in String
+// first 'GC' + 1 to 6 chars in a string, current is 5 (05.2012)
+// return String
+// example: http://jsfiddle.net/NUFGq/15/
+var findGCCodeFromString = function (str) {
+  if (!str || str.length === 0) { return false; }
+  var treffer = str.match(/\bGC([0-9A-Z]{1,6})\b/) || [];
+  return (treffer[0] || "");
 };
